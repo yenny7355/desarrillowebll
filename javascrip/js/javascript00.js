db15b4ff63445cdaac8f438e222fd4874a7fdcb8
estudiantegit <estudiante.git@gmail.com>
2024-04-16 08:21:12 -0400

    agregar una nueva carpeta y sus archivos

---
+++ javascrip/js/javascript00.js
@@ -0,0 +1,13 @@
+//comentario de una linea
+/*
+comentarios
+multiples
+lineas */
+/*
+Descripcion:ejemplo de hola mundo por condole.log
+desarrollador:nombre y apellido
+fecha:2024-04-15
+cambios:ninguno
+*/
+
+console.log("hola mundo externo,se vera?");
